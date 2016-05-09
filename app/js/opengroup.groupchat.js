ee0d13ccf298965578b956ac1030879c4199980f
Daniel Beeke <daniel@studiofonkel.nl>
2016-05-09 18:48:52 +0200

    yeoman start

--- app/js/opengroup.groupchat.js
+++
@@ -1,9 +0,0 @@
-OpenGroupPlugins["opengroup.groupchat"] = {
-    recieveMessage: function (message, openGroup, senderPeerId) {
-        console.log(message)
-    },
-    hooks: {
-        init: function () {
-        }
-    }
-};
